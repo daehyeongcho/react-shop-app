a7bb503b86add896398366b99fa4efb17ebd9f24
dhcho1034 <dhcho1034@gmail.com>
2021-06-05 19:34:39 +0900

    모든 상품 정보를 데이터베이스에 저장하기

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, Input } from "antd";
 import FileUpload from "../../utils/FileUpload";
+import Axios from "axios";
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -23,7 +24,7 @@ const continents = [
   { key: 7, value: "Antarctica" },
 ];
 
-export default function UploadProductPage() {
+export default function UploadProductPage(props) {
   const [inputs, setInputs] = useState(initialInputs);
   const { title, description, price, continent, images } = inputs;
   const onInputChange = (e) => {
@@ -36,13 +37,42 @@ export default function UploadProductPage() {
   const updateImages = (newImages) => {
     setInputs({ ...inputs, images: newImages });
   };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const hasNull = Object.keys(inputs).reduce((acc, name) => {
+      if (Array.isArray(inputs[name])) {
+        return acc || inputs[name].length === 0;
+      } else {
+        return acc && !inputs[name];
+      }
+    }, false);
+    if (hasNull) {
+      return alert("모든 값을 넣어주셔야 합니다.");
+    }
+    console.log("여기까지");
+    // 서버에 채운 값들을 request로 보낸다.
+    const body = {
+      // 로그인 된 사람의 ID
+      writer: props.user.userData._id,
+      ...inputs,
+    };
+
+    Axios.post("/api/product", body).then((response) => {
+      if (response.data.success) {
+        alert("상품 업로드에 성공했습니다.");
+        props.history.push("/");
+      } else {
+        alert("상품 업로드에 실패했습니다.");
+      }
+    });
+  };
 
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <Title level={2}> 여행 상품 업로드</Title>
       </div>
-      <Form>
+      <Form onSubmit={onSubmit}>
         {/* DropZone */}
         <FileUpload refreshFunction={updateImages} />
         <br />
@@ -77,7 +107,8 @@ export default function UploadProductPage() {
         </select>
         <br />
         <br />
-        <Button type="submit">확인</Button>
+        {/* 강의에선 type="submit"으로 했는데 작동안됨. onClick으로 해야함 */}
+        <Button onClick={onSubmit}>확인</Button>
       </Form>
     </div>
   );
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const { Product } = require("../models/Product");
 
 //=================================
 //             Product
@@ -16,6 +17,15 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage }).single("file");
+
+router.post("/", (req, res) => {
+  // 받아온 정보들을 DB에 넣어준다.
+  const product = new Product(req.body);
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 router.post("/image", (req, res) => {
   // 가져온 이미지를 저장을 해주면 된다.
