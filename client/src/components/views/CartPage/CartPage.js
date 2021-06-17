299c5ed7a940c92d2a5b0697a9be2f3f08865aba
dhcho1034 <dhcho1034@gmail.com>
2021-06-17 22:50:06 +0900

    Add to Cart 기능 개선 및 카트 Tab 만들기

---
+++ client/src/components/views/CartPage/CartPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CartPage() {
+  return (
+    <div>
+      CartPage
+      <br />
+      CartPage
+    </div>
+  );
+}
+
+export default CartPage;
--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { Menu } from "antd";
+import { Menu, Icon, Badge } from "antd";
 import axios from "axios";
 import { USER_SERVER } from "../../../Config";
 import { withRouter } from "react-router-dom";
@@ -19,6 +19,7 @@ function RightMenu(props) {
     });
   };
 
+  // 로그인 안됨
   if (user.userData && !user.userData.isAuth) {
     return (
       <Menu mode={props.mode}>
@@ -31,8 +32,19 @@ function RightMenu(props) {
       </Menu>
     );
   } else {
+    // 로그인 됨
     return (
       <Menu mode={props.mode}>
+        <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
+          <Badge count={5}>
+            <a href="/user/cart" style={{ marginRight: -22, color: "#667777" }}>
+              <Icon
+                type="shopping-cart"
+                style={{ fontSize: 30, marginBottom: 3 }}
+              />
+            </a>
+          </Badge>
+        </Menu.Item>
         <Menu.Item key="upload">
           <a href="/product/upload">Upload</a>
         </Menu.Item>
