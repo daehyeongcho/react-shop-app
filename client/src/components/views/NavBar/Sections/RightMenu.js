f171c9b86dcbb78c9da97ab832acb90fed58b904
dhcho1034 <dhcho1034@gmail.com>
2021-06-23 01:35:49 +0900

    Cart Badge 카운트 반영

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -36,7 +36,7 @@ function RightMenu(props) {
     return (
       <Menu mode={props.mode}>
         <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
-          <Badge count={5}>
+          <Badge count={user.userData && user.userData.cart.length}>
             <a href="/user/cart" style={{ marginRight: -22, color: "#667777" }}>
               <Icon
                 type="shopping-cart"
